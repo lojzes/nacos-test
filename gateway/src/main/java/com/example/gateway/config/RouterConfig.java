552da46d77dd4e97522a776da9446359fa1b7382
lojzes <lojzes@qq.com>
2019-08-19 18:28:29 +0800

    lyy

---
+++ gateway/src/main/java/com/example/gateway/config/RouterConfig.java
@@ -0,0 +1,10 @@
+package com.example.gateway.config;
+
+/**
+ * 描述：TODO
+ *
+ * @author lyyitit@foxmail.com
+ * @date 2019/8/19 13:51
+ */
+public class RouterConfig {
+}
--- nacos-provider/src/main/java/com/example/nacos/provider/controller/EchoController.java
+++ nacos-provider/src/main/java/com/example/nacos/provider/controller/EchoController.java
@@ -2,6 +2,7 @@ package com.example.nacos.provider.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -15,6 +16,16 @@ public class EchoController {
 
     @GetMapping("echo/{name}")
     public String echo(@PathVariable String name){
+        return "hello " + name;
+    }
+
+    @GetMapping("echo2")
+    public String echo2(@RequestParam("name") String name){
+        return "hello " + name;
+    }
+
+    @GetMapping("test")
+    public String test(@RequestParam("name") String name){
         return "hello " + name;
     }
 }
